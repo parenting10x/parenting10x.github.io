1a9e48bfa005634a2287f3ff4fdb513058411630
makinggamesinc <oliver_marsh@live.com.au>
2022-10-15 09:40:49 +1030

    changed tab title

--- html_writer/main.c
+++ html_writer/main.c
@@ -108,7 +108,7 @@ static void writeHeader(FileState *state) {
 	char *text = "<!DOCTYPE html>\
 	<html lang=\"en\">\
 		<head>\
-		  <title>Oliver Marsh</title>\
+		  <title>Parenting 10x app</title>\
 		  <meta charset=\"utf-8\">\
 		  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\
 		  <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">\
